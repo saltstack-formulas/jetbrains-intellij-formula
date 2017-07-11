2263baf8fb3152fa5b45f6f7f46120edc705c301
Noel McLoughlin <noel.mcloughlin@gmail.com>
2017-07-12 00:17:23 +0100

    Fix jinja variable (context set in init.sls)

--- intellij/intellij.sh
+++ intellij/intellij.sh
@@ -1,4 +1,2 @@
-{%- from 'intellij/settings.sls' import intellij with context %}
-
-export INTELLIJ_HOME={{ intellij.intellij_home }}
+export INTELLIJ_HOME={{ intellij_home }}
 export PATH=$INTELLIJ/bin:$PATH
