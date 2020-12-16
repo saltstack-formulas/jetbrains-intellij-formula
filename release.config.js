32c597805e7790d5116650ca585e1887b260c858
Imran Iqbal <iqbalmy@hotmail.com>
2020-12-16 06:33:12 +0000

    ci(gitlab-ci): use GitLab CI as Travis CI replacement
    * Automated using https://github.com/myii/ssf-formula/pull/275

--- release.config.js
+++ release.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   branch: 'master',
+  repositoryUrl: 'https://github.com/saltstack-formulas/jetbrains-intellij-formula',
   plugins: [
       ['@semantic-release/commit-analyzer', {
         preset: 'angular',
